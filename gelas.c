ec73c319c99b6cc80d9a8e0576e8361eeffac6d8
tariq <munawararsy@gmail.com>
2025-05-27 10:29:14 +0700

    updated code

--- gelas.c
+++ gelas.c
@@ -1,4 +1,40 @@
 #include <stdio.h>
 #include "gelas.h"
 
-void isiPenuh
+void isiPenuh(Gelas* g) {
+    g->isi = g->kapasitas;
+}
+
+void kosongkan(Gelas* g) {
+    g->isi = 0;
+}
+
+void isiDengan(Gelas* g, int volume) {
+    if (volume < 0) {
+        return;
+    }
+
+    g->isi = g->isi + volume;
+
+    if (g->isi > g->kapasitas) {
+        g->isi = g->kapasitas;
+    }
+}
+
+void tuangke(Gelas* dari, Gelas* ke) {
+    int ruangKosong = ke->kapasitas - ke->isi;
+
+    int jumlahYangDituang;
+    if (dari->isi <= ruangKosong) {
+        jumlahYangDituang = dari->isi;
+    } else {
+        jumlahYangDituang = ruangKosong;
+    }
+
+    dari->isi = dari->isi - jumlahYangDituang;
+    ke->isi = ke->isi + jumlahYangDituang;
+}
+
+void tampilkanStatus(const Gelas* g) {
+    printf("Gelas (kapasitas: %d, isi: %d)\n", g->kapasitas, g->isi);
+}
--- main.c
+++ main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
-#include "gelas.c"
+#include "gelas.h"
 
-int main(){
-    Gelas.kapasitas = 100;
-    Gelas.isi = 50;
-    
+int main() {
+    Gelas a = {10, 0};
+    Gelas b = {7, 5};
 
+    printf("Status awal:\n");
+    tampilkanStatus(&a);
+    tampilkanStatus(&b);
+
+    printf("\nIsi penuh gelas A:\n");
+    isiPenuh(&a);
+    tampilkanStatus(&a);
+
+    printf("\nTuang dari A ke B:\n");
+    tuangke(&a, &b);
+    tampilkanStatus(&a);
+    tampilkanStatus(&b);
+
+    printf("\nKosongkan gelas B:\n");
+    kosongkan(&b);
+    tampilkanStatus(&b);
+
+    printf("\nIsi gelas B dengan 4:\n");
+    isiDengan(&b, 4);
+    tampilkanStatus(&b);
+
+    return 0;
 }
